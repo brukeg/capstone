2f1bcc11b9907a51fb4cfa0c7867cd342664bdd1
brukeg <bruke@spreadlabel.com>
2019-05-08 21:33:00 -0700

    Adding draft of models and controler view logic

--- home/models.py
+++ home/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_handle = models.CharField(max_length=30)
+    first_nam = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=60)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    postal_code = models.CharField(max_length=10)
+    ssn = models.CharField(max_length=9)
+    dob = models.DateField()
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=10)
+    crypto_address = models.CharField(max_length=42)
+    entity_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.user_handle
+
+class SilaTransaction(models.Model):
+    issue_amount = models.IntegerField()
+    transfer_amount = models.IntegerField()
+    redeem_amount = models.IntegerField()
+
+class Transactions(models.Model):
+    pass
+    # transaction_object =
+
+class Response(models.Model):
+    pass
+    # response_object =
--- home/views.py
+++ home/views.py
@@ -1,14 +1,69 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import os
+import os, json
 from silasdk import App
 from silasdk import User as sila_user
 from silasdk import Transaction
 
 
+app_private_key = '7C857662051B2C2653A317B15DAFD31C84D214BB2C013087B9F64FA885367100'
+app_handle = 'brukeco.silamoney.eth'
+app = App("SANDBOX", app_private_key, app_handle)
 
 def index(request):
     app_private_key = '7C857662051B2C2653A317B15DAFD31C84D214BB2C013087B9F64FA885367100'
     app_handle = 'brukeco.silamoney.eth'
     silaApp = App("SANDBOX", app_private_key, app_handle)
-    return HttpResponse('silaApp.app_handle')
+    return HttpResponse(silaApp.app_handle)
+
+
+def check_handle(request):
+    if request.method == 'POST':
+        handle_input = request.POST['handle']
+        data = request.json
+        result = sila_user.checkHandle(app, data)
+        return result
+
+def register(request):
+    if request.method == 'POST':
+        register_fields = request.POST['register']
+        data = request.json
+        result = sila_user.register(app, data)
+        return result
+
+
+def request_kyc(request):
+    data = request.json
+    result = sila_user.requestKyc(app, data, data["private_key"])
+    return result
+
+
+def check_kyc(request):
+    data = request.json
+    result = sila_user.checkKyc(app, data, data["private_key"])
+    return result
+
+
+def link_account(request):
+    pass
+
+
+def get_accounts(request):
+    pass
+
+
+def issue_sila(request):
+    pass
+
+
+def transfer_sila(request):
+    pass
+
+
+def redeem_sila(request):
+    pass
+
+
+def get_transactions(request):
+    pass
+
