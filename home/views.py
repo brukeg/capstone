d62d3dac2f3389bba14f081ce7fc0adc8044ea35
brukeg <bruke@spreadlabel.com>
2019-05-16 20:36:57 -0700

    Finished register controller view logic. Checking in RequestKyc controller view logic.

--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
+# import os, json
 from django.shortcuts import render
-from django.http import HttpResponse
-import os, json
-from home.models import User_entity, Developer
+from django.conf import settings
+from .models import User_entity
+# from django.http import HttpResponse
+# from home.models import User_entity, Developer
 from silasdk import App
 from silasdk import User as sila_user
+from silasdk import EthWallet
 from silasdk import Transaction
-from django.conf import settings
+
 
 # app_private_key = 'B9*****************D0'
 app_handle = 'brukenodedemo_test.app.silamoney.eth'
@@ -23,10 +26,8 @@ def check_handle(request):
     if request.method == 'POST':
         handle_input = request.POST['handle']
         handle = handle_input + '.silamoney.eth'
-        print(handle)
         payload = {"user_handle": handle}
         response = sila_user.checkHandle(app, payload)
-        print(response)
         context = {'message': response['message']}
         return render(request, 'home/index.html', context=context)
     else:
@@ -35,43 +36,80 @@ def check_handle(request):
 def register(request):
     if request.method == 'POST':
         register_fields = request.POST.getlist('register')
-        print(register_fields)
-        country = "US"
         handle = register_fields[2] + '.silamoney.eth'
-        print(country, handle)
+        wallet = EthWallet.create("saoiufsidyfoisueyfjwhgerhwebrwbebrwemnbrauxkhamewskhx")
         payload = {
-            "country": country,
+            "country": "US",
             "user_handle": handle,
             "first_name": register_fields[0],
             "last_name": register_fields[1],
-            "entity_name": 'Last Family Trust',
+            "entity_name": register_fields[0] + ' ' + register_fields[1],
             "identity_value": register_fields[7],
-            "phone": '1234567890',
+            "phone": register_fields[10],
             "street_address_1": register_fields[3],
             "city": register_fields[4],
             "state": register_fields[5],
             "postal_code": register_fields[6],
-            "crypto_address": '0x7324F22D94e4F41d51903660fc5CbC60C31D4657',
-            "birthdate": register_fields[8]
+            "crypto_address": wallet['eth_address'],
+            "birthdate": register_fields[8],
             }
         response = sila_user.register(app, payload)
-        print(response)
-        context = {'message': response['message']}
+
+        user = User_entity(
+            country="US",
+            user_handle=handle,
+            first_name=register_fields[0],
+            last_name=register_fields[1],
+            entity_name=register_fields[0] + ' ' + register_fields[1],
+            identity_value=register_fields[7],
+            phone=register_fields[10],
+            street_address_1=register_fields[3],
+            city=register_fields[4],
+            state=register_fields[5],
+            postal_code=register_fields[6],
+            crypto_address=wallet['eth_address'],
+            birthdate=register_fields[8],
+            private_key=wallet['eth_private_key'],
+            email=register_fields[9],
+            )
+        user.save()
+        context = {
+            'reference': response['reference'],
+            'message': response['message'],
+            'status': response['status'],
+            'city': payload['city'],
+            'state': payload['state'],
+            'street': payload['street_address_1'],
+            'zip': payload['postal_code'],
+            'etheraddress': payload['crypto_address'],
+            'handle': payload['user_handle'],
+            }
         return render(request, 'home/index.html', context=context)
     else:
-        return render(request, 'home/index.html', context=context)
+        return render(request, 'home/index.html')
 
 
 def request_kyc(request):
-    data = request.json
-    result = sila_user.requestKyc(app, data, data["private_key"])
-    return result
+    if request.method == 'POST':
+        user_obj = User_entity.objects.latest('created_date')
+        request_kyc_payload = {"user_handle": user_obj.user_handle}
+        response = sila_user.requestKyc(app, request_kyc_payload, user_obj.private_key)
+        print(response)
+        return render(request, 'home/index.html')
+    else:
+        return render(request, 'home/index.html')
 
 
 def check_kyc(request):
-    data = request.json
-    result = sila_user.checkKyc(app, data, data["private_key"])
-    return result
+    if request.method == 'POST':
+        check = request.POST['check-kyc']
+        print(check_kyc)
+        check_kyc_payload = {"user_handle": User_entity.user_handle}
+        response = sila_user.checkKyc(app, check_kyc_payload, User_entity.private_key)
+        print(response)
+        return render(request, 'home/index.html')
+    else:
+        return render(request, 'home/index.html')
 
 
 def link_account(request):
