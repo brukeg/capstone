659857873aa03839d837b66363989ac3012ff192
brukeg <bruke@spreadlabel.com>
2019-05-16 20:38:18 -0700

    Updated User_entity to reflect neccessary additions: identity_value, street_address_1, country, and birthdate

--- home/models.py
+++ home/models.py
@@ -2,21 +2,22 @@ from django.db import models
 from django.utils import timezone
 
 class User_entity(models.Model):
-    user_handle = models.CharField(max_length=30)       # required: the end users Sila ENS handle
-    first_name = models.CharField(max_length=30)        # required: first name of the end user
-    last_name = models.CharField(max_length=30)         # required: last name of the end user
-    address = models.CharField(max_length=60)           # required: physical address of end users
-    city = models.CharField(max_length=30)              # required: the end users city
-    state = models.CharField(max_length=2)              # required: the state the end users makes their domicile
-    postal_code = models.CharField(max_length=10)       # required: USPS zip code for the end users
-    ssn = models.CharField(max_length=9)                # required: users social security number
-    dob = models.DateField()                            # required: users date of birth
-    email = models.EmailField(max_length=100)           # required: email address
-    phone = models.CharField(max_length=10)             # required: users US phone number
-    crypto_address = models.CharField(max_length=42)    # required: users ethereum address beginning with 0x
-    # entity_name = models.CharField(max_length=100)      # required:
-    private_key = models.CharField(max_length=256)      # required:
-    created_date = models.DateTimeField(default=timezone.now)
+    country = models.CharField(max_length=30, default='US')             # Required: US is the only supported country
+    user_handle = models.CharField(max_length=30)                       # required: the end users Sila ENS handle
+    first_name = models.CharField(max_length=30)                        # required: first name of the end user
+    last_name = models.CharField(max_length=30)                         # required: last name of the end user
+    street_address_1 = models.CharField(max_length=60)                  # required: physical address of end users
+    city = models.CharField(max_length=30)                              # required: the end users city
+    state = models.CharField(max_length=2)                              # required: the users state
+    postal_code = models.CharField(max_length=10)                       # required: USPS zip code for the end users
+    identity_value = models.CharField(max_length=9)                     # required: users social security number
+    birthdate = models.DateField()                                      # required: users date of birth
+    email = models.EmailField(max_length=100)                           # required: email address
+    phone = models.CharField(max_length=10)                             # required: users US phone number
+    crypto_address = models.CharField(max_length=42)                    # required: users ethereum address
+    entity_name = models.CharField(max_length=100)                      # required: users full name
+    private_key = models.CharField(max_length=256)                      # required: encrypted private key for address
+    created_date = models.DateTimeField(default=timezone.now)           # date user is created
 
     def __str__(self):
         """String for representing the Model object."""
