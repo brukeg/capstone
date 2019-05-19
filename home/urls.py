206a5cb60589205427b70fee57bcf6f55566bc67
brukeg <bruke@spreadlabel.com>
2019-05-19 16:12:15 -0700

    Reordered urls to reflect the correct sequence of api to call.

--- home/urls.py
+++ home/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('request_kyc/', views.request_kyc, name='request_kyc'),
     path('link_account/', views.link_account, name='link_account'),
     path('get_accounts/', views.get_accounts, name='get_accounts'),
+    path('get_transactions/', views.get_transactions, name='get_transactions'),
     path('issue_sila/', views.issue_sila, name='issue_sila'),
     path('transfer_sila/', views.transfer_sila, name='transfer_sila'),
     path('redeem_sila/', views.redeem_sila, name='redeem_sila'),
-    path('get_transactions/', views.get_transactions, name='get_transactions'),
 ]
